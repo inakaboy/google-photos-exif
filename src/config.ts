d8933e28cd320fa2054ac279c4515aac6780cc02
Daniel Lopez 👾 <daniel@6temes.cat>
2022-02-22 20:56:19 +0900

    Disable prettier in this project

--- src/config.ts
+++ src/config.ts
@@ -2,21 +2,21 @@ import { Config } from './models/config-models';
 
 export const CONFIG: Config = {
   supportedMediaFileTypes: [
-    { extension: '.avi',  supportsExif: false },
+    { extension: '.avi', supportsExif: false },
     { extension: '.dng', supportsExif: false },
-    { extension: '.gif',  supportsExif: false },
+    { extension: '.gif', supportsExif: false },
     { extension: '.heic', supportsExif: true },
     { extension: '.jpeg', supportsExif: true },
-    { extension: '.jpg',  supportsExif: true },
-    { extension: '.m4v',  supportsExif: false },
-    { extension: '.mov',  supportsExif: true },
-    { extension: '.mp4',  supportsExif: false },
-    { extension: '.png',  supportsExif: false },
-    { extension: '.webp',  supportsExif: false },
+    { extension: '.jpg', supportsExif: true },
+    { extension: '.m4v', supportsExif: false },
+    { extension: '.mov', supportsExif: true },
+    { extension: '.mp4', supportsExif: false },
+    { extension: '.png', supportsExif: false },
+    { extension: '.webp', supportsExif: false },
   ],
   localizedEditedSuffixes: [
-    "edited", // English
-    // "editat", // Catalan -- add yours here if your Google Photos was localized to a different language
+    'edited', // English
+    // 'editat', // Catalan -- add yours here if your Google Photos was localized to a different language
   ],
 };
 
