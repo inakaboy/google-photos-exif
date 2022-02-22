7fc90d3f1ef38d1a5762706e7cb3e07abdcf633d
Daniel Lopez 👾 <daniel@6temes.cat>
2022-02-22 21:01:49 +0900

    Add extra file types

--- src/config.ts
+++ src/config.ts
@@ -2,17 +2,20 @@ import { Config } from './models/config-models';
 
 export const CONFIG: Config = {
   supportedMediaFileTypes: [
-    { extension: '.avi', supportsExif: false },
-    { extension: '.dng', supportsExif: false },
-    { extension: '.gif', supportsExif: false },
+    { extension: '.3gp',  supportsExif: true },
+    { extension: '.avi',  supportsExif: false },
+    { extension: '.dng',  supportsExif: false },
+    { extension: '.gif',  supportsExif: false },
     { extension: '.heic', supportsExif: true },
     { extension: '.jpeg', supportsExif: true },
-    { extension: '.jpg', supportsExif: true },
-    { extension: '.m4v', supportsExif: false },
-    { extension: '.mov', supportsExif: true },
-    { extension: '.mp4', supportsExif: false },
-    { extension: '.png', supportsExif: false },
+    { extension: '.jpg',  supportsExif: true },
+    { extension: '.m4v',  supportsExif: false },
+    { extension: '.mkv',  supportsExif: true },
+    { extension: '.mov',  supportsExif: true },
+    { extension: '.mp4',  supportsExif: false },
+    { extension: '.png',  supportsExif: false },
     { extension: '.webp', supportsExif: false },
+
   ],
   localizedEditedSuffixes: [
     'edited', // English
